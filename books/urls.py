b99c44967248d31b5860fbb0abe54282a7a5ad08
quocthang0507 <quocthang_0507@yahoo.com.vn>
2021-09-21 13:50:33 +0700

    Update website

--- books/urls.py
+++ books/urls.py
@@ -2,6 +2,15 @@ from django.urls import path
 
 from . import views
 
+app_name = 'books'
 urlpatterns = [
-    path('', views.index, name='Index')
+    # /books/
+    path('', views.index, name='Index'),
+    # /books/5/
+    path('<int:book_id>/', views.detail, name='detail'),
+    # /books/5/table_content/
+    path('<int:book_id>/table_content/',
+         views.table_content, name='table_content'),
+    # /books/5/read/
+    path('<int:book_id>/read/', views.read, name='read'),
 ]
--- books/views.py
+++ books/views.py
@@ -1,7 +1,42 @@
-from django.http import HttpResponse
+import books
+from django.http import HttpResponse, Http404
+from django.template import loader
+from django.shortcuts import render, get_object_or_404
 
+from .models import Book
 # Create your views here.
 
 
+# def index(request):
+#     latest_book_list = Book.objects.order_by('-published_date')[:5]
+#     template = loader.get_template('books/index.html')
+#     context = {'latest_book_list': latest_book_list}
+#     return HttpResponse(template.render(context, request))
+
+
+# shortcut
 def index(request):
-    return HttpResponse('Hello, World. You\'re at the books index.')
+    latest_book_list = Book.objects.order_by('-published_date')[:5]
+    context = {'latest_book_list': latest_book_list}
+    return render(request, 'books/index.html', context)
+
+
+# def detail(request, book_id):
+#     try:
+#         book = Book.objects.get(pk=book_id)
+#     except Book.DoesNotExist:
+#         raise Http404('Book does not exist!')
+#     return render(request, 'books/detail.html', {'book': book})
+
+# shortcut
+def detail(request, book_id):
+    book = get_object_or_404(Book, pk=book_id)
+    return render(request, 'books/detail.html', {'book': book})
+
+
+def table_content(request, book_id):
+    return HttpResponse('You\'re looking at table of content %s.' % book_id)
+
+
+def read(request, book_id):
+    return HttpResponse('You\'re reading book %s.' % book_id)
